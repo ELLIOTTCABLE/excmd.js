b576eaded02721e73ceaf654e8d8be9f3d7a61a1
ELLIOTTCABLE <me@ell.io>
2018-08-23 18:07:57 -0700

    (- re test) Clean up lexer tests

--- test/lexer.test.js
+++ test/lexer.test.js
@@ -1,24 +1,27 @@
 const lexer = require('../src/lexer.bs')
 const tokens = require('../src/tokens.bs')
 
+let of_string = lexer.buffer_of_string
+
 test('lexes an EOF', ()=> {
-   const buf = lexer.buffer_of_string('')
+   const buf = of_string('')
    expect(lexer.next(buf)).toBe(tokens.eOF)
 })
 
 test('provides character-location information', ()=> {
                                    /* '01234' */
-   const buf = lexer.buffer_of_string(' ( ) ')
+   const buf = of_string(' ( ) ')
        , loc = lexer.next_loc(buf)
 
    expect(lexer.token(loc)).toBe(tokens.lEFT_PAREN)
    expect(lexer.start_cnum(loc)).toBe(1)
 })
 
+// FIXME: Inexplicably, Sedlex's line-counting doesn't seem to be working
 test.skip('provides line-location information', ()=> {
-   const buf = lexer.buffer_of_string("\n"   // 0
-      + " ( ) \n"                            // 1
-      + "")                                  // 2
+   const buf = of_string("\n"    // 0
+      + " ( ) \n"                // 1
+      + "")                      // 2
 
    const loc = lexer.next_loc(buf)
 
