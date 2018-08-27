bed6da96a9da6ad76767f8c440fe4e2815309823
ELLIOTTCABLE <me@ell.io>
2018-08-26 20:31:45 -0500

    (new NF) Expand on Unicode categorization

--- pkg/generate_uchar_ranges.ml
+++ pkg/generate_uchar_ranges.ml
@@ -16,6 +16,13 @@ let get_exn = function
   | Some x -> x
   | None   -> raise (Invalid_argument "Option.get")
 
+let name_of cp =
+   let i = Uchar.to_int cp in
+   match get_exn (Uucd.cp_prop ucd i Uucd.name) with
+   | `Name s -> s
+   | `Pattern pat -> pat
+
+
 (* This is about to get ugly. *)
 type state = {
    mutable curr : Uchar.t;
@@ -28,21 +35,68 @@ type state = {
    mutable viramas : Uchar.t list;
    mutable nonspacings : Uchar.t list;
    mutable non_reordered_nonspacings : Uchar.t list;
+
+   mutable vowel_dependents : Uchar.t list;
 }
 
-let s = {
-   curr = Uchar.max;
+let s : state = {
+   curr = Uchar.min;
+
    transparents = [];
    right_joins = [];
    left_joins = [];
+
+   letters = [];
+   viramas = [];
+   nonspacings = [];
+   non_reordered_nonspacings = [];
+
+   vowel_dependents = [];
 }
 
+type entry = Single of Uchar.t | Range of (Uchar.t * Uchar.t)
+
+let collapse (entries : entry list) (cp : Uchar.t) : entry list =
+   match entries with
+   | Range (a, b) :: rest ->
+      if (Uchar.pred a) = cp then
+         Range (cp, b) :: rest
+      else
+         Single cp :: Range (a, b) :: rest
+
+   | Single b :: rest ->
+      if (Uchar.pred b) = cp then
+         Range (cp, b) :: rest
+      else
+         Single cp :: Single b :: rest
+
+   | [] -> [Single cp]
+
+let dump_matchers lid chars =
+   let is_first = ref true in
+   Printf.printf "let %s = [%%sedlex.regexp?\n" lid;
+   (List.fold_left collapse [] chars)
+   |> (List.iter begin fun entry ->
+      if !is_first then begin
+         is_first := false;
+         print_string "  "
+      end else
+         print_string "| ";
+
+      match entry with
+      | Range (a, b) ->
+         Printf.printf "0x%04X .. 0x%04X (* '%s' - '%s' *)\n"
+            (Uchar.to_int a) (Uchar.to_int b) (name_of a) (name_of b)
+      | Single cp ->
+         Printf.printf "0x%04X (* '%s' *)\n" (Uchar.to_int cp) (name_of cp)
+   end);
+   print_string "]\n\n"
 
 let () =
-   try while (s.curr <- Uchar.pred s.curr; true) do
+   try while (s.curr <- Uchar.succ s.curr; true) do
       let i = Uchar.to_int s.curr in
 
-      if (i mod 0x1000) = 0 then Printf.eprintf "U+%04X\n" i;
+      if (i mod 0x1000) = 0 then Printf.eprintf "U+%04X\n%!" i;
 
       (* UAX #31-R1a, A1: http://unicode.org/reports/tr31/#A1 *)
       begin match Uucd.cp_prop ucd i Uucd.joining_type with
@@ -61,7 +115,7 @@ let () =
 
       (* UAX #31-R1a, A2: http://unicode.org/reports/tr31/#A2 *)
       let general_category = get_exn (Uucd.cp_prop ucd i Uucd.general_category)
-      and combining_class = Uucd.cp_prop ucd i Uucd.canonical_combining_class in
+      and combining_class = get_exn (Uucd.cp_prop ucd i Uucd.canonical_combining_class) in
       begin match (general_category, combining_class) with
       (* General_Category = Letter *)
       | (`Lu, _)
@@ -71,26 +125,56 @@ let () =
       | (`Lo, _) ->
             s.letters <- (s.curr :: s.letters)
 
-      (* General_Category = Nonspacing_Mark *)
-      | (`Mn, _) ->
+      (* General_Category = Nonspacing_Mark && Canonical_Combining_Class = Not_Reordered *)
+      | (`Mn, 0) ->
             s.nonspacings <- (s.curr :: s.nonspacings)
+
+      (* General_Category = Nonspacing_Mark && Canonical_Combining_Class != Not_Reordered *)
+      | (`Mn, _) ->
+            s.nonspacings <- (s.curr :: s.nonspacings);
+            s.non_reordered_nonspacings <- (s.curr :: s.non_reordered_nonspacings)
 
       (* Canonical_Combining_Class = Virama *)
       | (_, 9) ->
             s.viramas <- (s.curr :: s.viramas)
 
-      | Some _
-      | None -> ()
+      | _ -> ()
+      end;
+
+      (* UAX #31-R1a, B: http://unicode.org/reports/tr31/#B *)
+      begin match get_exn (Uucd.cp_prop ucd i Uucd.indic_syllabic_category) with
+      | `Vowel_Dependent ->
+            s.vowel_dependents <- (s.curr :: s.vowel_dependents)
+      | _ -> ()
       end;
 
 
    done with Invalid_argument _ ->
+      Printf.eprintf "\n>> Total characters matching each predicate ...\n";
       Printf.eprintf "Joining_Type = Transparent: %i\n" (List.length s.transparents);
+      Printf.eprintf "Joining_Type = Right: %i\n" (List.length s.right_joins);
       Printf.eprintf "Joining_Type = Left: %i\n" (List.length s.left_joins);
-      Printf.eprintf "Joining_Type = : %i\n" (List.length s.right_joins);
 
       Printf.eprintf "General_Category = Letter: %i\n" (List.length s.letters);
+      Printf.eprintf "General_Category = Nonspacing_Mark: %i\n" (List.length s.nonspacings);
+      Printf.eprintf "General_Category = Nonspacing_Mark, minus CCC=0: %i\n"
+         (List.length s.non_reordered_nonspacings);
+      Printf.eprintf "Canonical_Combining_Class = Virama: %i\n" (List.length s.viramas);
 
-      List.iter (fun cp ->
-         Printf.printf "U+%04X\n" (Uchar.to_int cp)
-      ) s.transparents
+      Printf.eprintf "Indic_Syllabic_Category = Vowel_Dependent: %i\n"
+         (List.length s.vowel_dependents);
+
+      Printf.eprintf "\n>> Printing matching clauses to stdout ...\n";
+
+      dump_matchers "joining_type_transparent" s.transparents;
+      dump_matchers "joining_type_right" s.right_joins;
+      dump_matchers "joining_type_left" s.left_joins;
+
+      dump_matchers "general_category_letter" s.letters;
+      dump_matchers "general_category_nonspacing_mark" s.nonspacings;
+      dump_matchers "general_category_nonspacing_mark_minus_not_reordered"
+         s.non_reordered_nonspacings;
+      dump_matchers "canonical_combining_class_virama" s.viramas;
+
+      dump_matchers "indic_syllabic_category_vowel_dependent" s.vowel_dependents;
+
