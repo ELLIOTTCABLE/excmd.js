6afc36e4a3b75d0ae129aa28a2418e1a6960d8d6
ELLIOTTCABLE <me@ell.io>
2018-11-11 14:58:20 -0600

    (new test) Require explicit non-terminal choice in the testing-tool

--- test/parser_test.ml
+++ test/parser_test.ml
@@ -2,5 +2,8 @@ open Excmd_parser
 
 let () =
    match Sys.argv with
-   | [| _; str |] -> AST.pp_statement (Parser.statement_in_string str)
-   | _ -> raise (Invalid_argument "Pass a single argument")
+   | [| _; "script"; str |] -> AST.pp (Parser.script_in_string str)
+   | [| _; "statement"; str |] -> AST.pp_statement (Parser.statement_in_string str)
+   | [| _; _; _ |] -> raise (Invalid_argument
+      "First argument must be a valid non-terminal (e.g. 'script' or 'statement')")
+   | _ -> raise (Invalid_argument "Two arguments are required")
