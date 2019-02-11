54198fef29588da23dcf2b6007f217f80f8cdd2c
ELLIOTTCABLE <git@ell.io>
2019-02-10 20:03:56 -0600

    (- re JSintf) Changing variable names in the interface

--- src/interface.js
+++ src/interface.js
@@ -3,68 +3,69 @@ import {
    fromFakeUTF8String,
    fixBrokenBuckleScriptUTF8String,
 } from '../src/fake_string'
-import $ from './lexer.bs'
+
+import $Lexer from './lexer.bs'
 
 // Used to ensure that nothing else can invoke these classes' constructors directly
 const INTERNAL = Symbol()
 
 export class LexBuffer {
-   constructor(is_internal, buf) {
+   constructor(is_internal, $buf) {
       if (is_internal !== INTERNAL)
-         throw new Error('`Buffer` must be constructed with `.of_string` et al.')
+         throw new Error('`Buffer` must be constructed with `LexBuffer.of_string()` et al.')
 
-      this.buf = buf
+      this.$buf = $buf
    }
 
    static of_string(string) {
       const utf8 = toFakeUTF8String(string),
-         buf = $.buffer_of_string(utf8)
-      return new LexBuffer(INTERNAL, buf)
+         $buf = $Lexer.buffer_of_string(utf8)
+      return new LexBuffer(INTERNAL, $buf)
    }
 
    next() {
-      const loc_tok = $.next_loc(this.buf)
-      return new Token(INTERNAL, loc_tok)
+      const $loc_tok = $Lexer.next_loc(this.$buf)
+      return new Token(INTERNAL, $loc_tok)
    }
 
    rest() {
-      return $.tokens_loc(this.buf).map(loc_tok => new Token(INTERNAL, loc_tok))
+      return $Lexer.tokens_loc(this.$buf).map($loc_tok => new Token(INTERNAL, $loc_tok))
    }
 }
 
 export class Token {
-   constructor(is_internal, loc_tok) {
+   constructor(is_internal, $loc_tok) {
       if (is_internal !== INTERNAL) throw new Error('`Token` cannot be constructed')
 
-      this.loc_tok = loc_tok
+      this.$loc_tok = $loc_tok
    }
 
-   get _raw() {
-      return $.token(this.loc_tok)
+   get $raw() {
+      return $Lexer.token(this.$loc_tok)
    }
 
    get id() {
-      return Symbol.for($.show_token(this._raw))
+      return Symbol.for($Lexer.show_token(this.$raw))
    }
 
    get start_line() {
-      return $.start_lnum(this.loc_tok)
+      return $Lexer.start_lnum(this.$loc_tok)
    }
    get start_idx() {
-      return $.start_cnum(this.loc_tok)
+      return $Lexer.start_cnum(this.$loc_tok)
    }
    get end_line() {
-      return $.end_lnum(this.loc_tok)
+      return $Lexer.end_lnum(this.$loc_tok)
    }
    get end_idx() {
-      return $.end_cnum(this.loc_tok)
+      return $Lexer.end_cnum(this.$loc_tok)
    }
 
    get body() {
-      return fixBrokenBuckleScriptUTF8String($.token_body(this._raw))
+      return fixBrokenBuckleScriptUTF8String($Lexer.token_body(this.$raw))
    }
 
    compare(other) {
-      return $.compare_token(this._raw, other._raw)
+      return $Lexer.compare_token(this.$raw, other.$raw)
    }
 }
