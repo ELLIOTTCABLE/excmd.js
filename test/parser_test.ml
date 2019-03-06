1d6bf51ae4bcb819e301c91431085595aa081b0f
ELLIOTTCABLE <git@ell.io>
2019-03-06 09:43:14 -0600

    (- test re) Clarify naming of ML-testing helpers

--- test/parser_test.ml
+++ test/parser_test.ml
@@ -1,16 +1,16 @@
 open Excmd
 
-let scrpt desc str =
+let test_script desc str =
    Printf.printf "\n\n### %s:\nParser.script %S\n" desc str ;
    AST.pp (Parser.script_of_string str)
 
 
-let stmt desc str =
+let test_statement desc str =
    Printf.printf "\n\n### %s:\nParser.statement %S\n" desc str ;
    Statement.pp (Parser.statement_of_string str)
 
 
-let incr_stmt desc str =
+let incremental_statement desc str =
    Printf.printf "\n\n### %s:\nIncremental.statement %S\n" desc str ;
    let pp stmt = Statement.dehydrate stmt |> Statement.pp in
    (pp, Incremental.statement_of_string str)
@@ -18,47 +18,54 @@ let incr_stmt desc str =
 
 let tests () =
    (* Basic statements *)
-   stmt "Simple, single command" "test" ;
-   stmt "Single command, with count" "2test" ;
+   test_statement "Simple, single command" "test" ;
+   test_statement "Single command, with count" "2test" ;
    (* Basic statements, with parameters *)
-   stmt "Single command, with single positional parameter" "test foo" ;
-   stmt "Single command, count, and single positional parameter" "2test foo" ;
-   stmt "Single command, with multiple positional parameters" "test foo bar" ;
-   stmt "Single command, count, and multiple positional parameters" "2test foo bar" ;
-   stmt "Single command, with single boolean flag" "test --foo" ;
-   stmt "Single command, with multiple boolean flags" "test --foo --bar" ;
-   stmt "Single command, with single boolean short-flag" "test -f" ;
-   stmt "Single command, with multiple, concatenated boolean short-flags" "test -foo" ;
-   stmt "Single command, with multiple, separated boolean short-flags" "test -f -o -o" ;
-   stmt "Single command, with single possibly-parameterized flag" "test --foo bar" ;
-   stmt "Single command, with single possibly-parameterized short-flag" "test -f bar" ;
-   stmt
+   test_statement "Single command, with single positional parameter" "test foo" ;
+   test_statement "Single command, count, and single positional parameter" "2test foo" ;
+   test_statement "Single command, with multiple positional parameters" "test foo bar" ;
+   test_statement "Single command, count, and multiple positional parameters"
+      "2test foo bar" ;
+   test_statement "Single command, with single boolean flag" "test --foo" ;
+   test_statement "Single command, with multiple boolean flags" "test --foo --bar" ;
+   test_statement "Single command, with single boolean short-flag" "test -f" ;
+   test_statement "Single command, with multiple, concatenated boolean short-flags"
+      "test -foo" ;
+   test_statement "Single command, with multiple, separated boolean short-flags"
+      "test -f -o -o" ;
+   test_statement "Single command, with single possibly-parameterized flag"
+      "test --foo bar" ;
+   test_statement "Single command, with single possibly-parameterized short-flag"
+      "test -f bar" ;
+   test_statement
       "Single command with single possibly-parameterized flag followed by a positional \
        parameter"
       "test --foo bar baz" ;
-   stmt "Single command with two possibly-parameterized flags"
+   test_statement "Single command with two possibly-parameterized flags"
       "test --foo bar --baz widget" ;
-   stmt "Single command, with single explicitly-parameterized flag" "test --foo=bar" ;
-   stmt
+   test_statement "Single command, with single explicitly-parameterized flag"
+      "test --foo=bar" ;
+   test_statement
       "Single command with single explicitly-parameterized flag followed by a positional \
        parameter"
       "test --foo=bar baz" ;
-   stmt "Single command with mixed flags and parameters"
+   test_statement "Single command with mixed flags and parameters"
       "test --foo bar --baz=widget qux -qu ux" ;
    (* Simple multi-statement scripts *)
-   scrpt "Statements separated by semicolons" "test; 2test; 3test" ;
-   scrpt "Statements separated by semicolons, with a trailing semicolon"
+   test_script "Statements separated by semicolons" "test; 2test; 3test" ;
+   test_script "Statements separated by semicolons, with a trailing semicolon"
       "test; 2test; 3test;" ;
-   scrpt "Statements, with arguments, separated by semicolons"
+   test_script "Statements, with arguments, separated by semicolons"
       "test --foo bar; 2test --foo=bar; 3test --foo bar" ;
-   scrpt "Statements, with arguments, separated by semicolons, with a trailing semicolon"
+   test_script
+      "Statements, with arguments, separated by semicolons, with a trailing semicolon"
       "test --foo bar; 2test --foo=bar; 3test --foo bar;" ;
-   scrpt "Newlines after statements" "test;\n   2test;\n   3test" ;
-   scrpt "Newlines after statements, with a trailing newline"
+   test_script "Newlines after statements" "test;\n   2test;\n   3test" ;
+   test_script "Newlines after statements, with a trailing newline"
       "test;\n   2test;\n   3test;\n   " ;
    (* Incremental API *)
    let pp, entrypoint =
-      incr_stmt "An acceptable statement, incrementally" "hello --where=world"
+      incremental_statement "An acceptable statement, incrementally" "hello --where=world"
    in
    let accept statement = pp statement in
    let fail _last_good _failing = failwith "parsing should have succeeded" in
