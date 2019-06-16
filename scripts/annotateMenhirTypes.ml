b7360d054a36ae6bcf06958a5dcc64683a3a16fc
ELLIOTTCABLE <git@ell.io>
2019-06-16 05:00:05 -0500

    (re meta) Reimplement Menhir type-annotation machinery due to fpottier/menhir#24
    This was a mess, holy lord. Details in the linked GitLab issue; but basically, Menhir's output is inexplicably broken UTF-8, which JavaScript tooling is "fixing" by substituting U+FFFD / UTF-8 \xEF\xBF\xBD. That, of course, completely breaks the automaton.
    
    Solution? Re-write all the manipulation using Buffers, at least for now. Now no part of the input-file gets round-tripped through String; and is treated as opaque data; only the search-terms are required to be valid UTF-8. (This is going to be a problem if the ppx tools ever support GADTs, though ... because I'll need regexen to match that, as I briefly had previously.)
    
    Luckily, this hack works. For now.
    
    /see https://gitlab.inria.fr/fpottier/menhir/issues/24

---
+++ scripts/annotateMenhirTypes.ml
@@ -0,0 +1,112 @@
+(** Given a pair of filenames as command-line arguments, this script will ... FIXME: NYD *)
+
+let linesToAnnotate =
+   [| ( "type token ="
+      , "\n\
+         [@@bs.deriving jsConverter] [@@deriving show { with_path = false }, to_yojson \
+         { optional = true }]" ) |]
+
+
+(* Why this is missing from [Node.Fs], I can't imagine. *)
+external readFileAsBufferSync : string -> Node.buffer = "readFileSync"
+[@@bs.val] [@@bs.module "fs"]
+
+external writeFileAsBufferSync : string -> Node.buffer -> unit = "writeFileSync"
+[@@bs.val] [@@bs.module "fs"]
+
+external bufferConcat
+   :  Node.buffer array
+      -> ?length:int
+      -> unit
+      -> Node.buffer
+   = "concat"
+   [@@bs.val] [@@bs.scope "Buffer"]
+
+external bufferIndexOf
+   :  Node.buffer
+      -> string
+      -> ?offset:int
+      -> ?encoding:([`ascii | `utf8 | `utf16le | `usc2 | `base64 | `latin1 | `binary | `hex][@bs.string
+                    ])
+      -> unit
+      -> int
+   = "indexOf"
+   [@@bs.send]
+
+external bufferSlice
+   :  Node.buffer
+      -> ?start:int
+      -> ?end_:int
+      -> unit
+      -> Node.buffer
+   = "slice"
+   [@@bs.send]
+
+external bufferLength : Node.buffer -> int = "length" [@@bs.get]
+
+let print_usage () =
+   print_endline "\nUsage: `node annotateMenhirTypes.bs.js <source>.ml <dest>.ml`\n"
+
+
+let src, dest =
+   (* Node.Path.resolve, inexplicably, takes *two* strings. *)
+   let resolve = Node.Path.resolve "" in
+   let args = Js.Array.slice ~start:2 ~end_:5 Sys.argv in
+   match args |> Js.Array.map resolve with
+    | [|src; dest|] -> (src, dest)
+    | _ ->
+      print_usage () ;
+      Js.Exn.raiseError "need exactly two arguments: a source, and a destination"
+
+
+(* FIXME: This is going to break, if Menhir starts having any extra whitespace. It *only*
+   handles direct bytestring indexing; nothing like regexes or globs ... *)
+let prependAfterFirstOccurence ~(input : Node.buffer) ~(matchAfter : string)
+      ~(prependBeforeFollowing : string) ~(addition : string)
+   =
+   let startIndex = bufferIndexOf input matchAfter () in
+   string_of_int startIndex |> print_endline ;
+   if startIndex == -1 then
+      Js.String.concatMany [|"'"; matchAfter; "' not found in input file"|] ""
+      |> Js.Exn.raiseError ;
+   let followingIndex =
+      bufferIndexOf input ~offset:startIndex prependBeforeFollowing ()
+   in
+   string_of_int followingIndex |> print_endline ;
+   if followingIndex == -1 then
+      Js.String.concatMany
+         [| "'"
+          ; prependBeforeFollowing
+          ; "' not found following '"
+          ; matchAfter
+          ; "' in input file" |]
+         ""
+      |> Js.Exn.raiseError ;
+
+   (* Report! *)
+   Js.String.concatMany
+      [| "Adding annotation ))))e "
+       ; string_of_int followingIndex
+       ; "( to '"
+       ; matchAfter
+       ; "' (byte "
+       ; string_of_int startIndex
+       ; ")" |]
+      ""
+   |> print_endline ;
+
+   (* Okay, have some numbers. Time to start using them to manipulate bytes. *)
+   let before = bufferSlice input ~end_:followingIndex () in
+   let after = bufferSlice input ~start:followingIndex () in
+   let length = bufferLength input + Js.String.length addition in
+   bufferConcat [|before; Node.Buffer.fromString addition; after|] ~length ()
+
+
+let () =
+   let contents = readFileAsBufferSync src in
+   let pafo input (matchAfter, addition) =
+      prependAfterFirstOccurence ~input ~matchAfter ~prependBeforeFollowing:"\n\n"
+         ~addition
+   in
+   let result = linesToAnnotate |> Js.Array.reduce pafo contents in
+   writeFileAsBufferSync dest result
