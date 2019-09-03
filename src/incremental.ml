641f9a8757b47765d7740c234b4808947563697d
ELLIOTTCABLE <git@ell.io>
2019-09-03 02:38:36 -0500

    (new MLAPI incr) Exposing LR-stack-element introspection

--- src/incremental.ml
+++ src/incremental.ml
@@ -13,6 +13,8 @@ module Incremental = ParserAutomaton.Incremental
 
 type 'a checkpoint = { status : 'a Interpreter.checkpoint; buf : Lexer.buffer }
 
+type element = ParserAutomaton.MenhirInterpreter.element
+
 type 'a t = Lexer.buffer -> 'a checkpoint
 
 let script buf =
@@ -78,7 +80,7 @@ let continue ~accept ~fail cp =
    Interpreter.loop_handle_undo accept fail supplier cp
 
 
-let current_command (cp : 'a checkpoint) =
+let current_command cp =
    let { status = menhir_cp } = cp in
    match menhir_cp with
     (* FIXME: Are all of these actually states in which I can't determine a command? *)
@@ -95,7 +97,7 @@ let current_command (cp : 'a checkpoint) =
       f 0
 
 
-let automaton_status_str (cp : 'a checkpoint) =
+let automaton_status_str cp =
    let { status = cp } = cp in
    match cp with
     | InputNeeded _env -> "InputNeeded"
@@ -113,7 +115,7 @@ let element_incoming_symbol_category_str = function
           | Interpreter.N _x -> "Nonterminal" )
 
 
-let incoming_symbol_category_str (cp : 'a checkpoint) =
+let incoming_symbol_category_str cp =
    let { status = cp } = cp in
    let the_env =
       match cp with
@@ -123,13 +125,16 @@ let incoming_symbol_category_str (cp : 'a checkpoint) =
        | AboutToReduce (env, _prod) -> env
        | HandlingError env -> env
        | Accepted _v ->
-         failwith "incoming_symbol_category: I don't know how to handle Accepted checkpoints"
+         failwith
+            "incoming_symbol_category: I don't know how to handle Accepted checkpoints"
        | Rejected ->
-         failwith "incoming_symbol_category: I don't know how to handle Rejected checkpoints"
+         failwith
+            "incoming_symbol_category: I don't know how to handle Rejected checkpoints"
    in
    match Interpreter.top the_env with
     | Some el -> Some (element_incoming_symbol_category_str el)
     | None -> None
+
 
 (* Ugggggggggggggggh literally copy-pasted this into existence out of compiled files,
    there HAS to be a better way to do this ... *)
@@ -137,64 +142,65 @@ let element_incoming_symbol_desc = function
    | Interpreter.Element (lr1state, _valu, _startp, _endp) -> (
          match Interpreter.incoming_symbol lr1state with
           | Interpreter.N Interpreter.N_unterminated_statement ->
-            "unterminated_statement", "AST.statement"
-          | Interpreter.N Interpreter.N_statement -> "statement", "AST.statement"
+            ("unterminated_statement", "AST.statement")
+          | Interpreter.N Interpreter.N_statement -> ("statement", "AST.statement")
           | Interpreter.N Interpreter.N_short_flags_before_positional ->
-            "short_flags_before_positional", "AST.arg list"
+            ("short_flags_before_positional", "AST.arg list")
           | Interpreter.N Interpreter.N_short_flags_before_flag ->
-            "short_flags_before_flag", "AST.arg list"
-          | Interpreter.N Interpreter.N_script -> "script", "AST.t"
+            ("short_flags_before_flag", "AST.arg list")
+          | Interpreter.N Interpreter.N_script -> ("script", "AST.t")
           | Interpreter.N Interpreter.N_positional_and_arguments ->
-            "positional_and_arguments", "AST.arg list"
+            ("positional_and_arguments", "AST.arg list")
           | Interpreter.N Interpreter.N_optterm_nonempty_list_break_unterminated_statement_ ->
-            "optterm_nonempty_list_break_unterminated_statement_", "AST.statement list"
+            ("optterm_nonempty_list_break_unterminated_statement_", "AST.statement list")
           | Interpreter.N Interpreter.N_optterm_list_break_unterminated_statement_ ->
-            "optterm_list_break_unterminated_statement_", "AST.statement list"
-          | Interpreter.N Interpreter.N_option_break_ ->
-            "option_break_", "unit option"
-          | Interpreter.N Interpreter.N_option_COUNT_ ->
-            "option_COUNT_", "string option"
+            ("optterm_list_break_unterminated_statement_", "AST.statement list")
+          | Interpreter.N Interpreter.N_option_break_ -> ("option_break_", "unit option")
+          | Interpreter.N Interpreter.N_option_COUNT_ -> ("option_COUNT_", "string option")
           | Interpreter.N Interpreter.N_nonempty_arguments ->
-            "nonempty_arguments", "AST.arg list"
-          | Interpreter.N Interpreter.N_noncommand_word ->
-            "noncommand_word", "string"
+            ("nonempty_arguments", "AST.arg list")
+          | Interpreter.N Interpreter.N_noncommand_word -> ("noncommand_word", "string")
           | Interpreter.N Interpreter.N_long_flag_before_positional ->
-            "long_flag_before_positional", "AST.arg"
+            ("long_flag_before_positional", "AST.arg")
           | Interpreter.N Interpreter.N_long_flag_before_flag ->
-            "long_flag_before_flag", "AST.arg"
-          | Interpreter.N Interpreter.N_list_COLON_ -> "list_COLON_", "unit list"
-          | Interpreter.N Interpreter.N_last_short_flags ->
-            "last_short_flags", "AST.arg list"
-          | Interpreter.N Interpreter.N_last_long_flag ->
-            "last_long_flag", "AST.arg"
+            ("long_flag_before_flag", "AST.arg")
+          | Interpreter.N Interpreter.N_list_COLON_ -> ("list_COLON_", "unit list")
+          | Interpreter.N Interpreter.N_last_short_flags -> ("last_short_flags", "AST.arg list")
+          | Interpreter.N Interpreter.N_last_long_flag -> ("last_long_flag", "AST.arg")
           | Interpreter.N Interpreter.N_flag_and_arguments ->
-            "flag_and_arguments", "AST.arg list"
-          | Interpreter.N Interpreter.N_command -> "command", "string"
-          | Interpreter.N Interpreter.N_break -> "break", "unit"
-          | Interpreter.N Interpreter.N_arguments -> "arguments", "AST.arg list"
-          | Interpreter.T Tokens.T_error -> "error", "unit"
-          | Interpreter.T Tokens.T_URL_START -> "URL_START", "string"
-          | Interpreter.T Tokens.T_URL_REST -> "URL_REST", "string"
-          | Interpreter.T Tokens.T_SEMICOLON -> "SEMICOLON", "unit"
-          | Interpreter.T Tokens.T_QUOTE_OPEN -> "QUOTE_OPEN", "string"
-          | Interpreter.T Tokens.T_QUOTE_ESCAPE -> "QUOTE_ESCAPE", "string"
-          | Interpreter.T Tokens.T_QUOTE_CLOSE -> "QUOTE_CLOSE", "string"
-          | Interpreter.T Tokens.T_QUOTE_CHUNK -> "QUOTE_CHUNK", "string"
-          | Interpreter.T Tokens.T_PIPE -> "PIPE", "unit"
-          | Interpreter.T Tokens.T_PAREN_OPEN -> "PAREN_OPEN", "unit"
-          | Interpreter.T Tokens.T_PAREN_CLOSE -> "PAREN_CLOSE", "unit"
-          | Interpreter.T Tokens.T_IDENTIFIER -> "IDENTIFIER", "string"
-          | Interpreter.T Tokens.T_FLAG_LONG -> "FLAG_LONG", "string"
-          | Interpreter.T Tokens.T_FLAGS_SHORT -> "FLAGS_SHORT", "string"
-          | Interpreter.T Tokens.T_EQUALS -> "EQUALS", "unit"
-          | Interpreter.T Tokens.T_EOF -> "EOF", "unit"
-          | Interpreter.T Tokens.T_COUNT -> "COUNT", "string"
-          | Interpreter.T Tokens.T_COMMENT_OPEN -> "COMMENT_OPEN", "unit"
-          | Interpreter.T Tokens.T_COMMENT_CLOSE -> "COMMENT_CLOSE", "unit"
-          | Interpreter.T Tokens.T_COMMENT -> "COMMENT", "string"
-          | Interpreter.T Tokens.T_COLON -> "COLON", "unit" )
+            ("flag_and_arguments", "AST.arg list")
+          | Interpreter.N Interpreter.N_command -> ("command", "string")
+          | Interpreter.N Interpreter.N_break -> ("break", "unit")
+          | Interpreter.N Interpreter.N_arguments -> ("arguments", "AST.arg list")
+          | Interpreter.T Tokens.T_error -> ("error", "unit")
+          | Interpreter.T Tokens.T_URL_START -> ("URL_START", "string")
+          | Interpreter.T Tokens.T_URL_REST -> ("URL_REST", "string")
+          | Interpreter.T Tokens.T_SEMICOLON -> ("SEMICOLON", "unit")
+          | Interpreter.T Tokens.T_QUOTE_OPEN -> ("QUOTE_OPEN", "string")
+          | Interpreter.T Tokens.T_QUOTE_ESCAPE -> ("QUOTE_ESCAPE", "string")
+          | Interpreter.T Tokens.T_QUOTE_CLOSE -> ("QUOTE_CLOSE", "string")
+          | Interpreter.T Tokens.T_QUOTE_CHUNK -> ("QUOTE_CHUNK", "string")
+          | Interpreter.T Tokens.T_PIPE -> ("PIPE", "unit")
+          | Interpreter.T Tokens.T_PAREN_OPEN -> ("PAREN_OPEN", "unit")
+          | Interpreter.T Tokens.T_PAREN_CLOSE -> ("PAREN_CLOSE", "unit")
+          | Interpreter.T Tokens.T_IDENTIFIER -> ("IDENTIFIER", "string")
+          | Interpreter.T Tokens.T_FLAG_LONG -> ("FLAG_LONG", "string")
+          | Interpreter.T Tokens.T_FLAGS_SHORT -> ("FLAGS_SHORT", "string")
+          | Interpreter.T Tokens.T_EQUALS -> ("EQUALS", "unit")
+          | Interpreter.T Tokens.T_EOF -> ("EOF", "unit")
+          | Interpreter.T Tokens.T_COUNT -> ("COUNT", "string")
+          | Interpreter.T Tokens.T_COMMENT_OPEN -> ("COMMENT_OPEN", "unit")
+          | Interpreter.T Tokens.T_COMMENT_CLOSE -> ("COMMENT_CLOSE", "unit")
+          | Interpreter.T Tokens.T_COMMENT -> ("COMMENT", "string")
+          | Interpreter.T Tokens.T_COLON -> ("COLON", "unit") )
 
-let incoming_symbol_type_str (cp : 'a checkpoint) =
+
+let element_incoming_symbol_type_str el =
+   match element_incoming_symbol_desc el with
+    | _name, typ -> typ
+
+
+let incoming_symbol_type_str cp =
    let { status = cp } = cp in
    let the_env =
       match cp with
@@ -209,13 +215,16 @@ let incoming_symbol_type_str (cp : 'a checkpoint) =
          failwith "incoming_symbol_type: I don't know how to handle Rejected checkpoints"
    in
    match Interpreter.top the_env with
-    | Some el -> begin
-          match element_incoming_symbol_desc el with
-           | (_name, typ) -> Some typ
-       end
+    | Some el -> Some (element_incoming_symbol_type_str el)
     | None -> None
 
-let incoming_symbol_str (cp : 'a checkpoint) =
+
+let element_incoming_symbol_str el =
+   match element_incoming_symbol_desc el with
+    | name, _typ -> name
+
+
+let incoming_symbol_str cp =
    let { status = cp } = cp in
    let the_env =
       match cp with
@@ -230,17 +239,14 @@ let incoming_symbol_str (cp : 'a checkpoint) =
          failwith "incoming_symbol: I don't know how to handle Rejected checkpoints"
    in
    match Interpreter.top the_env with
-    | Some el -> begin
-          match element_incoming_symbol_desc el with
-           | (name, _typ) -> Some name
-       end
+    | Some el -> Some (element_incoming_symbol_str el)
     | None -> None
 
 
 let element_incoming_symbol_desc_str el =
    let cat = element_incoming_symbol_category_str el in
    match element_incoming_symbol_desc el with
-    | name, typ -> String.concat "" [name; " : ("; typ; ") "; cat]
+    | name, typ -> String.concat "" [ name; " : ("; typ; ") "; cat ]
 
 
 let print_stack (env : 'a Interpreter.env) =
@@ -254,8 +260,35 @@ let print_stack (env : 'a Interpreter.env) =
    f 0
 
 
-let debug_checkpoint (cp : 'a checkpoint) =
+let debug_checkpoint cp =
    let { status = menhir_cp } = cp in
    match menhir_cp with
     | InputNeeded env -> print_stack env
     | _ -> failwith "supposed to be InputNeeded"
+
+
+let get_before cp idx =
+   let { status = cp } = cp in
+   let the_env =
+      match cp with
+       | InputNeeded env -> env
+       | Shifting (before, _after, _final) -> before
+       | AboutToReduce (env, _prod) -> env
+       | HandlingError env -> env
+       | Accepted _v ->
+         failwith "get_before: I don't know how to handle Accepted checkpoints"
+       | Rejected ->
+         failwith "get_before: I don't know how to handle Rejected checkpoints"
+   in
+   Interpreter.get idx the_env
+
+
+let get_after cp idx =
+   let { status = cp } = cp in
+   let the_env =
+      match cp with
+       | Shifting (_before, after, _final) -> after
+       | _ ->
+         failwith "get_after: This function is only relevant for Shifting checkpoints"
+   in
+   Interpreter.get idx the_env
--- src/incremental.mli
+++ src/incremental.mli
@@ -1,6 +1,8 @@
 type 'a checkpoint = private
    { status : 'a ParserAutomaton.MenhirInterpreter.checkpoint; buf : Lexer.buffer }
 
+type element = ParserAutomaton.MenhirInterpreter.element
+
 (** {2 Incremental entry-checkpoints} *)
 
 val script : Lexer.buffer -> AST.t checkpoint
@@ -17,7 +19,7 @@ val statement : Lexer.buffer -> AST.statement checkpoint
 val statement_of_string : string -> AST.statement checkpoint
 (** Helper to invoke {!val:statement} with a [string]. *)
 
-(** {2 Menhir incremental-parsing helpers} *)
+(** {2 Incremental actions} *)
 
 val continue
    :  accept:('semantic -> 'result)
@@ -26,7 +28,15 @@ val continue
    -> 'result
 (** A helper for Menhir's incremental parsers. Feeds the given ... NYD *)
 
-(** {2 Introspection} *)
+(** {2 High-level introspection helpers}
+
+    This set of functions are simply wrappers around Menhir's mechanics, to provide quick
+    access to a few derived values. Although one can easily implement additional
+    introspective tools in the same fashion, it's worth noting that any such helpers are
+    going to be tightly coupled to the specifics of the grammar; and such, it's probably a
+    good idea to keep them centrally located ... right here!
+
+    tl;dr upstream your usage, so I can excercise and standardize it, plz. *)
 
 val acceptable_token : 'a checkpoint -> Tokens.token
 (** [acceptable_token cp], given a [cp] that is in an {!InputNeeded} state, will return a
@@ -48,6 +58,12 @@ val current_command : 'a checkpoint -> string option
 
     JavaScript interface: [Checkpoint::command]. *)
 
+(** {2 Low-level introspection}
+
+    The rest of these mostly exist to unwrap and name types for the use of the JavaScript
+    interface; and are a pain to maintain. I really need automated generative tooling for
+    variant-to-string converters ... *)
+
 val automaton_status_str : 'a checkpoint -> string
 (** [automaton_status_str cp] will return the current
     {{:http://gallium.inria.fr/~fpottier/menhir/manual.html#sec59} Menhir
@@ -60,28 +76,41 @@ val automaton_status_str : 'a checkpoint -> string
 
     JavaScript interface: [Checkpoint::automaton_status]. *)
 
+val element_incoming_symbol_category_str : element -> string
+
 val incoming_symbol_category_str : 'a checkpoint -> string option
-(** [incoming_symbol_category_str cp] returns a string indicating whether the current symbol
-    (the top of the automaton's stack) is a ["terminal"] or ["nonterminal"] symbol. If the
-    stack is empty, this will produce [None].
+(** [incoming_symbol_category_str cp] returns a string indicating whether the current
+    symbol (the top of the automaton's stack) is a ["terminal"] or ["nonterminal"] symbol.
+    If the stack is empty, this will produce [None].
 
     This will raise an exception if called on [Accepted] or [Rejected] checkpoints.
 
     JavaScript interface: [Checkpoint::incoming_symbol_category]. *)
 
+val element_incoming_symbol_type_str : element -> string
+
 val incoming_symbol_type_str : 'a checkpoint -> string option
-(** [incoming_symbol_type_str cp] returns a string describing the OCaml runtime type of the semantic values associated with the current symbol (that on top of the automaton's stack). If the stack is empty, this will produce [None].
+(** [incoming_symbol_type_str cp] returns a string describing the OCaml runtime type of
+    the semantic values associated with the current symbol (that on top of the automaton's
+    stack). If the stack is empty, this will produce [None].
 
     This will raise an exception if called on [Accepted] or [Rejected] checkpoints.
 
     JavaScript interface: [Checkpoint::incoming_symbol_type]. *)
 
+val element_incoming_symbol_str : element -> string
+
 val incoming_symbol_str : 'a checkpoint -> string option
-(** [incoming_symbol_str cp] returns a string describing the current symbol (that on top of the automaton's stack); e.g. ["unterminated_statement"] or ["EQUALS"]. If the stack is empty, this will produce [None].
+(** [incoming_symbol_str cp] returns a string describing the current symbol (that on top
+    of the automaton's stack); e.g. ["unterminated_statement"] or ["EQUALS"]. If the stack
+    is empty, this will produce [None].
 
     This will raise an exception if called on [Accepted] or [Rejected] checkpoints.
 
     JavaScript interface: [Checkpoint::incoming_symbol]. *)
 
+val get_before : 'a checkpoint -> int -> element option
+
+val get_after : 'a checkpoint -> int -> element option
 
 val debug_checkpoint : 'a checkpoint -> unit
