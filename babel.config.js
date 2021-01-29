b112706b2401dcd2bf49675c5d81c984d629c092
ELLIOTTCABLE <git@ell.io>
2021-01-29 15:00:02 -0600

    (meta re) Configure Babel PTR to use ESM

--- babel.config.js
+++ babel.config.js
@@ -4,7 +4,15 @@ module.exports = {
       {
          test: 'packages/excmd/**/*',
          presets: ['@babel/env', '@babel/preset-typescript'],
-         plugins: ['@babel/plugin-transform-runtime'],
+         plugins: [
+            [
+               '@babel/plugin-transform-runtime',
+               {
+                  useESModules: true,
+                  version: '^7.8.3', // @babel/runtime version
+               },
+            ],
+         ],
       },
    ],
    babelrcRoots: ['.', './packages/*'],
