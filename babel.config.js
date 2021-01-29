1039576f8faefd5c990bf61f8565864a4c4aa2d8
ELLIOTTCABLE <git@ell.io>
2021-01-29 15:52:53 -0600

    (- meta fix) Lock down @babel/runtime version

--- babel.config.js
+++ babel.config.js
@@ -9,7 +9,12 @@ module.exports = {
                '@babel/plugin-transform-runtime',
                {
                   useESModules: true,
-                  version: '^7.8.3', // @babel/runtime version
+
+                  // NOTE: It's unclear if this value supports semver ranges or not; but I am
+                  //       choosing to duplicate the value from `package.json` precisely.
+                  //
+                  //       See: <https://github.com/babel/babel/blob/9808d2/packages/babel-plugin-transform-runtime/src/helpers.js#L4-L33>
+                  version: '~7.8.3', // @babel/runtime version
                },
             ],
          ],
